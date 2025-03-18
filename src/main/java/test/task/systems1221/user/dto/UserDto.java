f5a38d27af59ffb33eae1c09ea3d0ebbb6a09233
sergi-jr <burashnikov.sergei1@mail.ru>
2025-03-18 23:04:51 +0300

    add user dtos: create, common, update

--- src/main/java/test/task/systems1221/user/dto/UserDto.java
+++ src/main/java/test/task/systems1221/user/dto/UserDto.java
@@ -1,7 +1,23 @@
 package test.task.systems1221.user.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import test.task.systems1221.model.Goal;
+
+import java.util.UUID;
+
 /**
  * DTO for {@link test.task.systems1221.user.model.User}
  */
+@Getter
+@Setter
 public class UserDto {
+    private UUID id;
+    private String name;
+    private String email;
+    private int age;
+    private float weight;
+    private float height;
+    private Goal goal;
+    private int dailyRate;
 }
--- src/main/java/test/task/systems1221/user/dto/UserUpdateDto.java
+++ src/main/java/test/task/systems1221/user/dto/UserUpdateDto.java
@@ -1,7 +1,37 @@
 package test.task.systems1221.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+import test.task.systems1221.model.Goal;
+
 /**
  * DTO for {@link test.task.systems1221.user.model.User}
  */
+@Getter
+@Setter
 public class UserUpdateDto {
+    @Size(min = 1, max = 32)
+    @NotBlank(message = "Field cannot be empty")
+    private String name;
+
+    @Email(message = "Wrong email format",
+            regexp = "^[a-zA-Z0-9_+&*-] + (?:\\\\.[a-zA-Z0-9_+&*-] + )*@(?:[a-zA-Z0-9-]+\\\\.) + [a-zA-Z]{2,7}")
+    private String email;
+
+    @Positive
+    @Range(max = 120)
+    private int age;
+
+    @Positive
+    private float weight;
+
+    @Positive
+    private float height;
+
+    private Goal goal;
 }
