b1814207421c2c0431c8d857a052948957c54a61
sergi-jr <burashnikov.sergei1@mail.ru>
2025-03-18 23:03:31 +0300

    add nullable and reference mapper

--- src/main/java/test/task/systems1221/mapper/ReferenceMapper.java
+++ src/main/java/test/task/systems1221/mapper/ReferenceMapper.java
@@ -1,4 +1,20 @@
 package test.task.systems1221.mapper;
 
-public class ReferenceMapper {
+import jakarta.persistence.EntityManager;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.TargetType;
+import org.springframework.beans.factory.annotation.Autowired;
+import test.task.systems1221.model.BaseEntity;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING
+)
+public abstract class ReferenceMapper {
+    @Autowired
+    private EntityManager manager;
+
+    public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> clazz) {
+        return id != null ? manager.find(clazz, id) : null;
+    }
 }
