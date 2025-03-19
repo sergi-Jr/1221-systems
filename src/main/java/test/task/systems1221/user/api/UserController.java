c9e342a788d610ee027ba382e4884db974eec7ed
sergi-jr <burashnikov.sergei1@mail.ru>
2025-03-19 18:12:14 +0300

    fix valid annotation

--- src/main/java/test/task/systems1221/user/api/UserController.java
+++ src/main/java/test/task/systems1221/user/api/UserController.java
@@ -1,34 +1,30 @@
 package test.task.systems1221.user.api;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import test.task.systems1221.user.dal.UserService;
 import test.task.systems1221.user.dto.UserCreateDto;
 import test.task.systems1221.user.dto.UserDto;
 import test.task.systems1221.user.dto.UserUpdateDto;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/api/users", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
@@ -54,12 +50,12 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UserDto create(@RequestBody @Validated UserCreateDto dto) {
+    public UserDto create(@RequestBody @Valid UserCreateDto dto) {
         return userService.create(dto);
     }
 
     @PatchMapping("/{id}")
-    public UserDto patch(@PathVariable UUID id, @RequestBody @Validated UserUpdateDto userUpdateDto) {
+    public UserDto patch(@PathVariable UUID id, @RequestBody @Valid UserUpdateDto userUpdateDto) {
         return userService.patch(id, userUpdateDto);
     }
 
