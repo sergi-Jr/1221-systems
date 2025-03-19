488dd2f849fec01fdc76197869cae9b72f1ffd35
sergi-jr <burashnikov.sergei1@mail.ru>
2025-03-19 18:10:57 +0300

    add init sql schemas for different db vendors

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -5,15 +5,16 @@ CREATE TABLE users (
    name VARCHAR NOT NULL,
    email VARCHAR NOT NULL,
    age INTEGER NOT NULL,
-   weight FLOAT NOT NULL,
-   height FLOAT NOT NULL,
+   weight INTEGER NOT NULL,
+   height INTEGER NOT NULL,
    goal VARCHAR NOT NULL,
+   sex VARCHAR NOT NULL,
    daily_rate INTEGER NOT NULL,
    CONSTRAINT pk_users PRIMARY KEY (id)
 );
 
 ALTER TABLE users ADD CONSTRAINT uc_users_email UNIQUE (email);
 
-INSERT INTO users (id, name, email, age, weight, height, goal, daily_rate) VALUES (RANDOM_UUID(), 'test1', 'test1@mail.ru', 10, 35.5, 140, 'MAINTENANCE', 100);
-INSERT INTO users (id, name, email, age, weight, height, goal, daily_rate) VALUES (RANDOM_UUID(), 'test2', 'test2@mail.ru', 30, 100, 180, 'GAIN', 100);
-INSERT INTO users (id, name, email, age, weight, height, goal, daily_rate) VALUES (RANDOM_UUID(), 'test3', 'test3@mail.ru', 28, 80, 190, 'SLIMMING', 100);
+INSERT INTO users (id, name, email, age, weight, height, goal, daily_rate, sex) VALUES (RANDOM_UUID(), 'test1', 'test1@mail.ru', 10, 35, 140, 'MAINTENANCE', 100, 'MALE');
+INSERT INTO users (id, name, email, age, weight, height, goal, daily_rate, sex) VALUES (RANDOM_UUID(), 'test2', 'test2@mail.ru', 30, 100, 180, 'GAIN', 100, 'MALE');
+INSERT INTO users (id, name, email, age, weight, height, goal, daily_rate, sex) VALUES (RANDOM_UUID(), 'test3', 'test3@mail.ru', 28, 80, 190, 'SLIMMING', 100, 'FEMALE');
