04b213f53b5037575835fe69425d5d66dc364613
sergi-jr <burashnikov.sergei1@mail.ru>
2025-03-19 21:13:15 +0300

    add dish prduction method

--- src/test/java/test/task/systems1221/utils/EntityGenerator.java
+++ src/test/java/test/task/systems1221/utils/EntityGenerator.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import net.datafaker.Faker;
 import org.instancio.Instancio;
 import org.springframework.stereotype.Component;
+import test.task.systems1221.dish.model.Dish;
 import test.task.systems1221.model.Goal;
 import test.task.systems1221.model.Sex;
 import test.task.systems1221.user.model.User;
@@ -27,6 +28,17 @@ public class EntityGenerator {
                 .supply(field(User::getHeight), () -> faker.number().numberBetween(110, 250))
                 .supply(field(User::getGoal), () -> Goal.MAINTENANCE)
                 .supply(field(User::getSex), () -> Sex.MALE)
+                .create();
+    }
+
+    public Dish dish() {
+        return Instancio.of(Dish.class)
+                .ignore(field(Dish::getId))
+                .supply(field(Dish::getName), () -> faker.food().dish())
+                .supply(field(Dish::getCalorieAmount), () -> faker.number().numberBetween(50, 1000))
+                .supply(field(Dish::getProtein), () -> faker.number().numberBetween(5, 150))
+                .supply(field(Dish::getFat), () -> faker.number().numberBetween(5, 300))
+                .supply(field(Dish::getCarbohydrate), () -> faker.number().numberBetween(10, 500))
                 .create();
     }
 }
