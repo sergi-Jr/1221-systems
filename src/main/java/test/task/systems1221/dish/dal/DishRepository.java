d5b0afa0b2d051b77d6ca6d931172a86e3fd9199
sergi-jr <burashnikov.sergei1@mail.ru>
2025-03-19 22:01:02 +0300

    add by name search

--- src/main/java/test/task/systems1221/dish/dal/DishRepository.java
+++ src/main/java/test/task/systems1221/dish/dal/DishRepository.java
@@ -3,7 +3,9 @@ package test.task.systems1221.dish.dal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import test.task.systems1221.dish.model.Dish;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DishRepository extends JpaRepository<Dish, UUID> {
+    Optional<Dish> findByName(String name);
 }
