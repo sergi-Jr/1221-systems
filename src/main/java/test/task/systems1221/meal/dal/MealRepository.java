8a5b71711e19a8ab45fc2c2500945a11de9b3b28
sergi-jr <burashnikov.sergei1@mail.ru>
2025-03-21 15:14:47 +0300

    add meal repository

--- src/main/java/test/task/systems1221/meal/dal/MealRepository.java
+++ src/main/java/test/task/systems1221/meal/dal/MealRepository.java
@@ -1,4 +1,25 @@
 package test.task.systems1221.meal.dal;
 
-public interface MealRepository extends org.springframework.data.jpa.repository.JpaRepository<test.task.systems1221.meal.model.Meal, test.task.systems1221.meal.model.CompositeMealKey> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import test.task.systems1221.meal.dto.MealInfo;
+import test.task.systems1221.meal.model.Meal;
+
+import java.time.LocalDate;
+import java.util.UUID;
+import java.util.List;
+
+public interface MealRepository extends JpaRepository<Meal, Long> {
+
+    @Query("""
+            select new test.task.systems1221.meal.dto.MealInfo(SUM(d.calorieAmount * m.dishAmount), MAX(m.mealId))
+            from Meal m
+            join Dish d on m.dishId = d.id
+            where m.user.id = :userId and m.date = :date
+            """)
+    MealInfo getMealInfoByUserIdAndDate(UUID userId, LocalDate date);
+
+    List<Meal> findMealByUserIdAndDate(UUID userId, LocalDate date);
+
+    List<Meal> findByUserIdAndDateBetween(UUID userId, LocalDate dateStart, LocalDate dateEnd);
 }
