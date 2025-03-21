6c42ce467c3461411765f833d3261243292ef223
sergi-jr <burashnikov.sergei1@mail.ru>
2025-03-21 15:16:10 +0300

    add meal model

--- src/main/java/test/task/systems1221/meal/model/Meal.java
+++ src/main/java/test/task/systems1221/meal/model/Meal.java
@@ -1,7 +1,42 @@
-package test.task.systems1221.meal;
+package test.task.systems1221.meal.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import test.task.systems1221.model.BaseEntity;
+import test.task.systems1221.user.model.User;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
 @Entity
-public class Meal {
+@Table(name = "meals")
+public class Meal implements BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int mealId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private UUID dishId;
+
+    @Column(nullable = false)
+    private int dishAmount;
+
+    private LocalDate date;
 }
