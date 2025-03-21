277a2f2ecf2a2ed3ad159e854e81cf88ebd4149e
sergi-jr <burashnikov.sergei1@mail.ru>
2025-03-21 15:15:59 +0300

    add meal mapper

--- src/main/java/test/task/systems1221/meal/mapper/MealMapper.java
+++ src/main/java/test/task/systems1221/meal/mapper/MealMapper.java
@@ -1,4 +1,13 @@
 package test.task.systems1221.meal.mapper;
 
-public class MealMapper {
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+import test.task.systems1221.mapper.JsonNullableMapper;
+import test.task.systems1221.mapper.ReferenceMapper;
+
+@Mapper(uses = {JsonNullableMapper.class, ReferenceMapper.class},
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING)
+public abstract class MealMapper {
 }
